4972013129b454293d96826ef0bd8c6fd49dd95d
njugunacynthia32-sketch <njugunacynthia32@gmail.com>
2026-06-16 05:03:13 +0300

    complete cash register

--- cash_register.py
+++ cash_register.py
@@ -1,49 +1,50 @@
+#!/usr/bin/env python3
+
 class CashRegister:
-    def __init__(self, discount=0):
-        self.discount = discount
-        self.total = 0
-        self.items = []
-        self.previous_transactions = []
+  def __init__(self, discount=0):
+    self.discount = discount
+    self.total = 0
+    self.items = []
+    self.previous_transactions = []
 
-    @property
-    def discount(self):
-        return self._discount
+  @property
+  def discount(self):
+    return self._discount
 
-    @discount.setter
-    def discount(self, value):
-        if not isinstance(value, int) or not (0 <= value <= 100):
-            print("Not valid discount")
-            self._discount = 0
-        else:
-            self._discount = value
+  @discount.setter
+  def discount(self, value):
+    if not isinstance(value, int):
+      print('Not valid discount')
+      self._discount = 0
+    elif not (0<= value <=100):
+      print('Not valid discount')
+    else:
+      self._discount = value  
+            
 
-    def add_item(self, item, price, quantity=1):
-        self.total += price * quantity
-        self.items.extend([item] * quantity)
+  def add_item(self, item, price, quantity=1):
 
-        self.previous_transactions.append({
-            "item": item,
-            "price": price,
-            "quantity": quantity
-        })
+    self.total += price * quantity
+    self.items.extend([item] * quantity)
+    self.previous_transactions.append({'item': item, 'price': price, 'quantity':quantity})
 
-    def apply_discount(self):
-        if self.discount == 0:
-            print("There is no discount to apply.")
-            return
+    
 
-        self.total = self.total - (self.total * self.discount / 100)
+  def apply_discount(self):
+    if self.discount == 0:
+      print('There is no discount to apply.')
+    else:
+      self.total = self.total - (self.total * self.discount / 100)
+      print(f'After the discount, the total comes to ${int(self.total)}.')  
 
-        print(f"After the discount, the total comes to ${int(self.total)}.")
+    
 
-    def void_last_transaction(self):
-        if not self.previous_transactions:
-            print("There is no transaction to void.")
-            return
-
-        last = self.previous_transactions.pop()
-
-        self.total -= last["price"] * last["quantity"]
-
-        for _ in range(last["quantity"]):
-            self.items.remove(last["item"])
+  def void_last_transaction(self): 
+    if not self.previous_transactions:
+      print("There is no discount to apply.")
+    else:
+      last = self.previous_transactions.pop()
+      self.total -= last['price'] * last['quantity']
+      for _ in range(last['quantity']):
+        self.items.remove(last['item'])
+        
